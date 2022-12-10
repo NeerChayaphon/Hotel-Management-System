d63ec47ec000af27a4f03f1a770ad0cb8ec436bc
men1164 <41180013+men1164@users.noreply.github.com>
2022-12-10 19:30:19 +0700

    fix import

--- src/main/java/com/ooadproject/hotelmanagement/model/Room.java
+++ src/main/java/com/ooadproject/hotelmanagement/model/Room.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.annotation.Id;
 
 @Document(collection = "room")
 @Data
